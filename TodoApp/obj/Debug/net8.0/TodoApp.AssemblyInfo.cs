1f84225f5ffde463ea508e754651d4c8ce8098e3
DIBYAJYOTI2601 <dibyajyoti266012004@gmail.com>
2025-07-15 16:02:58 +0530

    another pull req

--- TodoApp/obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
+++ TodoApp/obj/Debug/net8.0/TodoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e44676db2acb374ac3d384a6998b751c584da54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+626caff67f48a31ef79f000850e9def9e8034cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
