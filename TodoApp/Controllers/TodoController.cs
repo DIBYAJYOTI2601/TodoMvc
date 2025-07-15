1e44676db2acb374ac3d384a6998b751c584da54
DIBYAJYOTI2601 <dibyajyoti266012004@gmail.com>
2025-07-15 12:38:52 +0530

    first commit

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+        [HttpGet]
+        public IActionResult TodoList()
+        {
+            var todos = _context.Todos.ToList();
+            return View(todos);
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTask(Todo newTask)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Todos.Add(newTask);
+                _context.SaveChanges();
+                return RedirectToAction("TodoList");
+            }
+
+            return View(newTask);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateTask(int Id)
+        {
+            var todo = _context.Todos.Find(Id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return View(todo);
+        }
+        [HttpPost]
+        public IActionResult UpdateTask(Todo todo,int Id)
+        {
+            if(Id!=todo.TodoId)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Todos.Update(todo);
+                _context.SaveChanges();
+                return RedirectToAction("TodoList");
+            }
+            return View(todo);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteTask(int Id)
+        {
+            var todo = _context.Todos.Find(Id);
+            if (todo == null) {
+                return NotFound();
+            }
+
+            _context.Todos.Remove(todo);
+            _context.SaveChanges();
+            return RedirectToAction("TodoList");
+        }
+        
+       
+        
+
+    }
+}
---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int TodoId { get; set; }
+
+        [MinLength(4,ErrorMessage = "too short")]
+        [Required(ErrorMessage = "Task name is required")]
+        public string TaskName { get; set; }
+
+        [Required(ErrorMessage = "Task description is required")]
+        [MinLength(5, ErrorMessage = "Task description must be at least 10 characters long")]
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+
+
+
+    }
+}
